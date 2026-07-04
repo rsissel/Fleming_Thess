27f1eb8354a29af3ab0bb7810a63123a30d8f097
rsissel <rsissel@gmail.com>
2026-07-04 19:53:43 +0300

    Add Next.js configuration for GitHub Pages

---
+++ next.js
@@ -0,0 +1,5 @@
+const nextConfig = {
+  output: 'export',
+  basePath: '/Fleming_Thess', // 👈 This tells Next.js to format all links to match your GitHub URL
+  // ... leave the rest of your existing settings below this
+};
